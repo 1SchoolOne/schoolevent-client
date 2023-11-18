8dc2493564938c62437dae203867a81880cfd7c1
Ilhan YAPICI <ilhan-yapici@outlook.fr>
2023-11-18 16:08:12 +0100

    feat: working login page

---
+++ src/components/Login/_components/LoginForm/LoginForm-types.ts
@@ -0,0 +1,4 @@
+export interface ILoginFormFields {
+	email?: string
+	password?: string
+}
--- src/components/Login/_components/LoginForm/LoginForm.tsx
+++ src/components/Login/_components/LoginForm/LoginForm.tsx
@@ -1,11 +1,43 @@
 import { Button, Form, Input } from 'antd'
+import { useNavigate } from 'react-router-dom'
+
+import { useSupabase } from '@utils/useSupabase'
+
+import { ILoginFormFields } from './LoginForm-types'
 
 export function LoginForm() {
 	const [form] = Form.useForm()
+	const supabase = useSupabase()
+	const navigate = useNavigate()
+
+	const onFinish = async (values: ILoginFormFields) => {
+		const { email, password } = values
+
+		if (!email || !password) return
+
+		const { error } = await supabase.auth.signInWithPassword({ email, password })
+
+		if (error) {
+			console.error(error)
+			return
+		}
+
+		form.resetFields()
+
+		navigate('/')
+	}
 
 	return (
-		<Form form={form} name="login" size="large" layout="vertical" autoComplete="off">
-			<Form.Item
+		<Form
+			form={form}
+			name="login"
+			size="large"
+			layout="vertical"
+			autoComplete="off"
+			onFinish={onFinish}
+			initialValues={{ email: '', password: '' }}
+		>
+			<Form.Item<ILoginFormFields>
 				label="Email"
 				name="email"
 				rules={[{ required: true, message: 'Veuillez saisir votre email.' }]}
@@ -13,7 +45,7 @@ export function LoginForm() {
 				<Input />
 			</Form.Item>
 
-			<Form.Item
+			<Form.Item<ILoginFormFields>
 				label="Mot de passe"
 				name="password"
 				rules={[{ required: true, message: 'Veuillez saisir votre mot de passe.' }]}

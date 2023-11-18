ffe0fb50f16a5b135e12033b4691cca8241df48c
Ilhan YAPICI <ilhan-yapici@outlook.fr>
2023-11-18 15:16:06 +0100

    fix: rename env variables

--- src/utils/useSupabase.ts
+++ src/utils/useSupabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseUrl = process.env.VITE_SUPABASE_URL
 
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+const supabaseKey = process.env.VITE_SUPABASE_KEY
 
 export function useSupabase() {
 	if (!supabaseUrl || !supabaseKey) throw new Error('Missing Supabase URL or Key')

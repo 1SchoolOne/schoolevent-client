c08961a488a11072105921b05737eff020e03ded
IlhanYapici <ilhan.yapici@outlook.fr>
2023-12-09 00:05:33 +0100

    fix: typo in localStorage doc

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -3,14 +3,14 @@ import localSotrage from 'store2'
 const STORAGE_PREFIX = 'se'
 
 /**
- * Hook version of store2. It will automatically be prefixed by 'SE_'.
+ * Hook version of store2. It will automatically be prefixed by 'se'
  */
 export function useLocalStorage() {
 	return localSotrage.namespace(STORAGE_PREFIX)
 }
 
 /**
- * Function version of store2. It will automatically be prefixed by 'SE_'.
+ * Function version of store2. It will automatically be prefixed by 'se'
  */
 export function getLocalStorage() {
 	return localSotrage.namespace(STORAGE_PREFIX)

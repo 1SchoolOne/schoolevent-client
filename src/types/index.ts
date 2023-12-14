e0be5d4e11433c75b3f471efbd687f5a5b80aa43
IlhanYapici <ilhan.yapici@outlook.fr>
2023-12-14 20:16:58 +0100

    config: generate supabase types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react'
 
+export type { Database } from './supabase'
+
 export type PropsWithOptionalChildren<P = object> = P & { children?: ReactNode }
 
 export type PropsWithChildren<P = object> = P & { children: ReactNode }

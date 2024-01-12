eba2f57cafb63a1a8561d1bad9d0dde082bf0443
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-01-12 16:43:04 +0100

    Fix: Multiple Error Correction

---
+++ src/components/Calendar/CalendarLayout.tsx
@@ -0,0 +1,22 @@
+import { Layout } from 'antd'
+
+import { Calendar } from './_components'
+
+import './CalendarLayout-styles.less'
+
+const { Content, Header, Sider } = Layout
+
+export function CalendarLayout() {
+	return (
+		<Layout className="calendar-layout">
+			<Header>Calendrier</Header>
+			<Layout>
+				<Sider width={250}>
+				</Sider>
+				<Content>
+					<Calendar />
+				</Content>
+			</Layout>
+		</Layout>
+	)
+}
---
+++ src/components/Calendar/_components/index.ts
@@ -0,0 +1,2 @@
+//export { CalendarList } from './CalendarList/CalendarList'
+export { Calendar } from './Calendar/Calendar'

e9d894999602f4df19689c668c229805954dedbf
IlhanYapici <ilhan-yapici@outlook.fr>
2024-01-12 20:43:03 +0100

    feat: save sidebar state in local storage

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,17 +6,4 @@ export type PropsWithOptionalChildren<P = object> = P & { children?: ReactNode }
 
 export type PropsWithChildren<P = object> = P & { children: ReactNode }
 
-export type TStorageKey = 'contacts.table' | 'contacts.favorites'
-
-export interface ITableStorage {
-	orderBy: string | null
-	paginationSize: number
-}
-
-interface ISetTableStorage {
-	key: 'contacts.table'
-	data: ITableStorage
-}
-
-export type TSetStorageParams = ISetTableStorage
-export type TSetStorageReturn = ITableStorage | null
+export * from './localStorage'

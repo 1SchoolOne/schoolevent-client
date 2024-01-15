95765f2ee8d5acd62653cbfb55537e0a32002b0b
IlhanYapici <ilhan.yapici@outlook.fr>
2024-01-15 19:36:29 +0100

    fix: lint geolocation-types.tsx

--- src/components/Map/_components/GeoLocation/GeoLocation-utils.tsx
+++ src/components/Map/_components/GeoLocation/GeoLocation-utils.tsx
@@ -2,46 +2,44 @@
 import { useEffect, useState } from 'react'
 
 // Interface
-import { IGeoLocationState, IPositionError } from './GeoLocation-types';
+import { IGeoLocationState, IPositionError } from './GeoLocation-types'
 
-export function GeoLocation(){
+export function GeoLocation() {
+	const [location, setLocation] = useState<IGeoLocationState>({
+		loaded: false,
+		geoLocationCoordinates: { lat: 0, lng: 0 },
+		error: null,
+	})
 
-  const [location, setLocation] = useState<IGeoLocationState>({
-    loaded: false,
-    geoLocationCoordinates: { lat: 0, lng: 0 },
-    error: null
-  });
+	const onSuccess = (location: { coords: { latitude: number; longitude: number } }) => {
+		setLocation({
+			loaded: true,
+			geoLocationCoordinates: {
+				lat: location.coords.latitude,
+				lng: location.coords.longitude,
+			},
+			error: null,
+		})
+	}
 
-  const onSuccess = (location: { coords: { latitude: number; longitude: number; }; }) => {
-    setLocation({
-      loaded: true,
-      geoLocationCoordinates: {
-        lat: location.coords.latitude,
-        lng: location.coords.longitude,
-      },
-      error: null
-    });
-  };
+	const onError = (error: IPositionError) => {
+		setLocation({
+			loaded: true,
+			geoLocationCoordinates: { lat: 0, lng: 0 },
+			error,
+		})
+	}
 
-  const onError = (error: IPositionError) => {
-    setLocation({
-      loaded: true,
-      geoLocationCoordinates: { lat: 0, lng: 0 },
-      error,
-    })
-  }
+	useEffect(() => {
+		if (!('geolocation' in navigator)) {
+			onError({
+				code: 0,
+				message: 'Geolocation not supported',
+			})
+		}
 
-  useEffect(() => {
-    if(!("geolocation" in navigator)) {
-      onError({
-        code: 0,
-        message: "Geolocation not supported",
-      });
-    }
+		navigator.geolocation.getCurrentPosition(onSuccess, onError)
+	}, [])
 
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-
-  }, [])
-  
-  return location;
+	return location
 }

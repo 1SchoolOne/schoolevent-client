b1df4ab003d4ffa0435f5c2efbfe2f7985d75539
Nicolas9578 <93204465+NicolasOlivier@users.noreply.github.com>
2024-01-16 10:46:02 +0100

    add folder event

--- src/routes/events/events.tsx
+++ src/routes/events/events.tsx
@@ -4,6 +4,8 @@ import { Outlet, RouteObject } from 'react-router-dom'
 import { ProtectedRoute } from '@components'
 import { eventFormRoute } from '@routes'
 
+import { EventList } from '../../components/events/eventsList/EventList'
+
 export const eventsRoute: RouteObject = {
 	path: 'events',
 	element: (
@@ -11,6 +13,7 @@ export const eventsRoute: RouteObject = {
 			<Helmet>
 				<title>SchoolEvent | Évènements</title>
 			</Helmet>
+			<EventList />
 			<Outlet />
 		</ProtectedRoute>
 	),

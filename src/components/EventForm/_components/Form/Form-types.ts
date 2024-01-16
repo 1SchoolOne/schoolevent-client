05d456c8f0bcfc46e430fc4290130685983121a6
Nicolas9578 <93204465+NicolasOlivier@users.noreply.github.com>
2024-01-16 10:46:02 +0100

    add folder event

--- src/components/EventForm/_components/Form/Form-types.ts
+++
@@ -1,3 +0,0 @@
-export interface IFormProps {
-	initialValues?: string
-}
--- src/components/events/type.ts
+++ src/components/events/type.ts
@@ -1,11 +1,14 @@
 import { TEventTypeValue } from './EventForm/EventForm-types'
 
 export interface IEventFormFields {
+	id: string
 	event_date: Date
-	event_name: string
-	event_position: string
+	event_title: string
+	event_adress: string
 	event_background?: { file: File; fileList: FileList }
-	event_time: string
+	event_duration: string
 	event_type: TEventTypeValue
 	event_description: string
+	event_creator_id: string
+	event_school_name: string
 }

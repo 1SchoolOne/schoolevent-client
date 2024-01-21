840bc26ff367e7b738dfe39a128d65b7254b3df7
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-01-21 15:07:54 +0100

    fix: Modification of the code to improve its readability and fix bugs

--- src/components/Calendar/_components/Calendar/Calendar.tsx
+++ src/components/Calendar/_components/Calendar/Calendar.tsx
@@ -1,64 +1,28 @@
-import { useState, useMemo, useCallback } from 'react'
-
-import { Badge, Button, ConfigProvider, Modal, Select } from 'antd'
 import { LeftOutlined, RightOutlined } from '@ant-design/icons'
+import { Badge, Button, ConfigProvider, Modal, Select } from 'antd'
 import { Calendar as AntCalendar } from 'antd'
 import { BadgeProps } from 'antd/lib/badge'
 import frFR from 'antd/lib/locale/fr_FR'
-
 import dayjs, { Dayjs } from 'dayjs'
 import 'dayjs/locale/fr'
+import { useCallback, useMemo, useState } from 'react'
 
-interface Appointment {
-	name: string
-	type: string
-	description: string
-	position: string
-	date: string
-	participant: string
-}
-
-const appointments: Appointment[] = [
-	{
-		type: 'test',
-		name: 'Rencontre Etudiante Esiee-it',
-		description: 'Rencontre avec plein d étudiants.',
-		position: 'Pontoise',
-		date: '2024-01-10',
-		participant: 'Nicolas',
-	},
-	{
-		type: 'test',
-		name: 'JPO ESCP',
-		description: 'Rencontre avec les étudiants pour les lycées.',
-		position: 'Paris',
-		date: '2024-01-30',
-		participant: 'Nicolas',
-	},
-	{
-		type: 'test',
-		name: 'Salon SUP de Vente',
-		description: 'Salon avec des entrepreneurs et des patrons.',
-		position: 'Pontoise',
-		date: '2024-01-27',
-		participant: 'Nicolas',
-	},
-]
+import { Appointment, appointments } from '../appointment.tsx'
 
 const Calendar = () => {
-	const [isModalVisible, setIsModalVisible] = useState(false);
-	const [selectedAppointment, setSelectedAppointment] = useState<Appointment | null>(null);
-	const [currentDate, setCurrentDate] = useState(dayjs());
+	const [isModalVisible, setIsModalVisible] = useState(false)
+	const [selectedAppointment, setSelectedAppointment] = useState<Appointment | null>(null)
+	const [currentDate, setCurrentDate] = useState(dayjs())
 
-	const currentYear = dayjs().year();
-	const years = useMemo(() => Array.from({length: 6}, (_, i) => currentYear + i), [currentYear]);
+	const currentYear = dayjs().year()
+	const years = useMemo(() => Array.from({ length: 6 }, (_, i) => currentYear + i), [currentYear])
 
 	const getListData = (value: Dayjs) => {
 		return appointments.filter((appointment) => {
-			const appointmentDate = dayjs(appointment.date);
-			return appointmentDate.isSame(value, 'day');
-		});
-	};
+			const appointmentDate = dayjs(appointment.date)
+			return appointmentDate.isSame(value, 'day')
+		})
+	}
 
 	const showAppointmentDetails = (appointment: Appointment) => {
 		setSelectedAppointment(appointment)
@@ -79,81 +43,97 @@ const Calendar = () => {
 	}
 
 	const handlePrevMonth = () => {
-		setCurrentDate(currentDate.subtract(1, 'month'));
-	};
-
-	const handleNextMonth = () => {
-		setCurrentDate(currentDate.add(1, 'month'));
-	};
-
-	const handleYearSelectChange = useCallback((newYear: string) => {
-		const updatedValue = currentDate.clone().year(Number(newYear));
-		setCurrentDate(updatedValue);
-	}, [currentDate, setCurrentDate]);
-
-	const handleMonthSelectChange = useCallback((newMonth: string) => {
-		const updatedValue = currentDate.clone().month(Number(newMonth));
-		setCurrentDate(updatedValue);
-	}, [currentDate, setCurrentDate]);
-
-	interface HeaderRenderParams {
-		value: Dayjs;
-		type: string;
-		onChange: (value: Dayjs) => void;
+		setCurrentDate(currentDate.subtract(1, 'month'))
 	}
 
-	const headerRender = useCallback(({ value }: HeaderRenderParams) => {
-		const start = 0;
-		const end = 12;
-		const monthOptions = [];
+	const handleNextMonth = () => {
+		setCurrentDate(currentDate.add(1, 'month'))
+	}
 
-		const current = value.clone();
-		const months = [];
-		for (let i = 0; i < 12; i++) {
-			months.push(current.locale('fr').month(i).format('MMM'));
-		}
+	const handleYearSelectChange = useCallback(
+		(newYear: string) => {
+			const updatedValue = currentDate.clone().year(Number(newYear))
+			setCurrentDate(updatedValue)
+		},
+		[currentDate, setCurrentDate],
+	)
 
-		for (let index = start; index < end; index++) {
-			monthOptions.push(
-				<Select.Option className="month-item" key={`${index}`}>
-					{months[index]}
-				</Select.Option>,
-			);
-		}
+	const handleMonthSelectChange = useCallback(
+		(newMonth: string) => {
+			const updatedValue = currentDate.clone().month(Number(newMonth))
+			setCurrentDate(updatedValue)
+		},
+		[currentDate, setCurrentDate],
+	)
 
-		return (
-			<div style={{ padding: 20, display: 'flex', alignItems: 'center' }}>
-				<Button size="small" onClick={handlePrevMonth}>
-					<LeftOutlined />
-				</Button>
-				<Select
-					size="small"
-					dropdownMatchSelectWidth={false}
-					className="my-year-select"
-					onChange={handleYearSelectChange}
-					value={String(currentDate.year())}
-				>
-					{years.map(year => (
-						<Select.Option key={year} value={String(year)}>
-							{year}
-						</Select.Option>
-					))}
-				</Select>
-				<Select
-					size="small"
-					dropdownMatchSelectWidth={false}
-					className="my-month-select"
-					onChange={handleMonthSelectChange}
-					value={String(currentDate.month())}
-				>
-					{monthOptions}
-				</Select>
-				<Button size="small" onClick={handleNextMonth}>
-					<RightOutlined />
-				</Button>
-			</div>
-		);
-	}, [handlePrevMonth, handleNextMonth, handleYearSelectChange, handleMonthSelectChange, currentDate, years]);
+	interface HeaderRenderParams {
+		value: Dayjs
+		type: string
+		onChange: (value: Dayjs) => void
+	}
+
+	const headerRender = useCallback(
+		({ value }: HeaderRenderParams) => {
+			const start = 0
+			const end = 12
+			const monthOptions = []
+
+			const current = value.clone()
+			const months = []
+			for (let i = 0; i < 12; i++) {
+				months.push(current.locale('fr').month(i).format('MMM'))
+			}
+
+			for (let index = start; index < end; index++) {
+				monthOptions.push(
+					<Select.Option className="month-item" key={`${index}`}>
+						{months[index]}
+					</Select.Option>,
+				)
+			}
+
+			return (
+				<div style={{ padding: 20, display: 'flex', alignItems: 'center' }}>
+					<Button size="small" onClick={handlePrevMonth}>
+						<LeftOutlined />
+					</Button>
+					<Select
+						size="small"
+						dropdownMatchSelectWidth={false}
+						className="my-year-select"
+						onChange={handleYearSelectChange}
+						value={String(currentDate.year())}
+					>
+						{years.map((year) => (
+							<Select.Option key={year} value={String(year)}>
+								{year}
+							</Select.Option>
+						))}
+					</Select>
+					<Select
+						size="small"
+						dropdownMatchSelectWidth={false}
+						className="my-month-select"
+						onChange={handleMonthSelectChange}
+						value={String(currentDate.month())}
+					>
+						{monthOptions}
+					</Select>
+					<Button size="small" onClick={handleNextMonth}>
+						<RightOutlined />
+					</Button>
+				</div>
+			)
+		},
+		[
+			handlePrevMonth,
+			handleNextMonth,
+			handleYearSelectChange,
+			handleMonthSelectChange,
+			currentDate,
+			years,
+		],
+	)
 
 	const handleCancel = () => {
 		setIsModalVisible(false)
@@ -178,7 +158,7 @@ const Calendar = () => {
 						<p>Name: {selectedAppointment.name}</p>
 						<p>Description: {selectedAppointment.description}</p>
 						<p>Position: {selectedAppointment.position}</p>
-						<p>Date: {selectedAppointment.date}</p>
+						<p>Date: {selectedAppointment.date.format('DD/MM/YYYY')}</p>
 						<p>Participant: {selectedAppointment.participant}</p>
 					</div>
 				)}

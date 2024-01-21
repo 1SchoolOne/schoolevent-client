f09d50326a79f646cb5b809d3b8d09b372a6ec81
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-01-21 14:38:40 +0100

    Fix: Bug fixes and code optimization

--- src/components/Calendar/_components/Calendar/Calendar.tsx
+++ src/components/Calendar/_components/Calendar/Calendar.tsx
@@ -1,21 +1,22 @@
-import { useState } from 'react';
-import { Calendar as AntCalendar, Badge, Modal, ConfigProvider, Button ,BadgeProps } from 'antd';
-import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import { useState, useMemo, useCallback } from 'react'
 
-import 'dayjs/locale/fr';
-import dayjs, { Dayjs } from 'dayjs';
-import frFR from 'antd/lib/locale/fr_FR';
+import { Badge, Button, ConfigProvider, Modal, Select } from 'antd'
+import { LeftOutlined, RightOutlined } from '@ant-design/icons'
+import { Calendar as AntCalendar } from 'antd'
+import { BadgeProps } from 'antd/lib/badge'
+import frFR from 'antd/lib/locale/fr_FR'
 
-import './Calendar-styles.less';
+import dayjs, { Dayjs } from 'dayjs'
+import 'dayjs/locale/fr'
 
-type Appointment = {
-	type: string;
-	name: string;
-	description: string;
-	position: string;
-	date: string;
-	participant: string;
-};
+interface Appointment {
+	name: string
+	type: string
+	description: string
+	position: string
+	date: string
+	participant: string
+}
 
 const appointments: Appointment[] = [
 	{
@@ -24,7 +25,7 @@ const appointments: Appointment[] = [
 		description: 'Rencontre avec plein d étudiants.',
 		position: 'Pontoise',
 		date: '2024-01-10',
-		participant: 'Nicolas'
+		participant: 'Nicolas',
 	},
 	{
 		type: 'test',
@@ -32,7 +33,7 @@ const appointments: Appointment[] = [
 		description: 'Rencontre avec les étudiants pour les lycées.',
 		position: 'Paris',
 		date: '2024-01-30',
-		participant: 'Nicolas'
+		participant: 'Nicolas',
 	},
 	{
 		type: 'test',
@@ -40,52 +41,130 @@ const appointments: Appointment[] = [
 		description: 'Salon avec des entrepreneurs et des patrons.',
 		position: 'Pontoise',
 		date: '2024-01-27',
-		participant: 'Nicolas'
-
+		participant: 'Nicolas',
 	},
-];
+]
 
 const Calendar = () => {
 	const [isModalVisible, setIsModalVisible] = useState(false);
 	const [selectedAppointment, setSelectedAppointment] = useState<Appointment | null>(null);
+	const [currentDate, setCurrentDate] = useState(dayjs());
+
+	const currentYear = dayjs().year();
+	const years = useMemo(() => Array.from({length: 6}, (_, i) => currentYear + i), [currentYear]);
 
 	const getListData = (value: Dayjs) => {
-		return appointments.filter(appointment => {
-			const [year, month, day] = appointment.date.split('-');
-			return value.date() === parseInt(day, 10) && value.month() === parseInt(month, 10) - 1 && value.year() === parseInt(year, 10);
+		return appointments.filter((appointment) => {
+			const appointmentDate = dayjs(appointment.date);
+			return appointmentDate.isSame(value, 'day');
 		});
 	};
 
+	const showAppointmentDetails = (appointment: Appointment) => {
+		setSelectedAppointment(appointment)
+		setIsModalVisible(true)
+	}
+
 	const dateCellRender = (value: Dayjs) => {
-		const listData = getListData(value);
+		const listData = getListData(value)
 		return (
 			<ul className="events">
-				{listData.map(item => (
+				{listData.map((item) => (
 					<li key={item.name} onClick={() => showAppointmentDetails(item)}>
 						<Badge status={item.type as BadgeProps['status']} text={item.name} />
 					</li>
 				))}
 			</ul>
-		);
+		)
+	}
+
+	const handlePrevMonth = () => {
+		setCurrentDate(currentDate.subtract(1, 'month'));
 	};
 
-	const showAppointmentDetails = (appointment: Appointment) => {
-		setSelectedAppointment(appointment);
-		setIsModalVisible(true);
+	const handleNextMonth = () => {
+		setCurrentDate(currentDate.add(1, 'month'));
 	};
+
+	const handleYearSelectChange = useCallback((newYear: string) => {
+		const updatedValue = currentDate.clone().year(Number(newYear));
+		setCurrentDate(updatedValue);
+	}, [currentDate, setCurrentDate]);
+
+	const handleMonthSelectChange = useCallback((newMonth: string) => {
+		const updatedValue = currentDate.clone().month(Number(newMonth));
+		setCurrentDate(updatedValue);
+	}, [currentDate, setCurrentDate]);
+
+	interface HeaderRenderParams {
+		value: Dayjs;
+		type: string;
+		onChange: (value: Dayjs) => void;
+	}
+
+	const headerRender = useCallback(({ value }: HeaderRenderParams) => {
+		const start = 0;
+		const end = 12;
+		const monthOptions = [];
+
+		const current = value.clone();
+		const months = [];
+		for (let i = 0; i < 12; i++) {
+			months.push(current.locale('fr').month(i).format('MMM'));
+		}
+
+		for (let index = start; index < end; index++) {
+			monthOptions.push(
+				<Select.Option className="month-item" key={`${index}`}>
+					{months[index]}
+				</Select.Option>,
+			);
+		}
+
+		return (
+			<div style={{ padding: 20, display: 'flex', alignItems: 'center' }}>
+				<Button size="small" onClick={handlePrevMonth}>
+					<LeftOutlined />
+				</Button>
+				<Select
+					size="small"
+					dropdownMatchSelectWidth={false}
+					className="my-year-select"
+					onChange={handleYearSelectChange}
+					value={String(currentDate.year())}
+				>
+					{years.map(year => (
+						<Select.Option key={year} value={String(year)}>
+							{year}
+						</Select.Option>
+					))}
+				</Select>
+				<Select
+					size="small"
+					dropdownMatchSelectWidth={false}
+					className="my-month-select"
+					onChange={handleMonthSelectChange}
+					value={String(currentDate.month())}
+				>
+					{monthOptions}
+				</Select>
+				<Button size="small" onClick={handleNextMonth}>
+					<RightOutlined />
+				</Button>
+			</div>
+		);
+	}, [handlePrevMonth, handleNextMonth, handleYearSelectChange, handleMonthSelectChange, currentDate, years]);
 
 	const handleCancel = () => {
-		setIsModalVisible(false);
-	};
+		setIsModalVisible(false)
+	}
 
 	return (
 		<ConfigProvider locale={frFR}>
-			<AntCalendar dateCellRender={dateCellRender} header={(headerProps) => (
-					<div>
-						<Button type="text" icon={<LeftOutlined />} onClick={() => headerProps.onPrev()} />
-						<Button type="text" icon={<RightOutlined />} onClick={() => headerProps.onNext()} />
-					</div>
-				)}
+			<AntCalendar
+				headerRender={headerRender}
+				value={currentDate}
+				dateCellRender={dateCellRender}
 			/>
 			<Modal
 				title="Les détails de l'événement :"
@@ -104,9 +183,8 @@ const Calendar = () => {
 					</div>
 				)}
 			</Modal>
-
 		</ConfigProvider>
-	);
-};
+	)
+}
 
-export { Calendar };
+export { Calendar }

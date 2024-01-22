b27c7f3340520e50d3912248ce26350ed7de12c1
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-01-22 10:16:52 +0100

    fix: Changes to the appointment list display: modals and design

--- src/components/Calendar/_components/CalendarList/CalendarList.tsx
+++ src/components/Calendar/_components/CalendarList/CalendarList.tsx
@@ -1,14 +1,25 @@
 import { MagnifyingGlass as SearchIcon } from '@phosphor-icons/react'
-import { Input, List, Space } from 'antd'
+import { Flex, Input, List, Modal, Space } from 'antd'
 import 'dayjs/locale/fr'
 import { useState } from 'react'
 
-import { appointments } from '../appointment'
+import { Appointment, appointments } from '../appointment'
 
 import './CalendarList-styles.less'
 
 export function CalendarList() {
 	const [search, setSearch] = useState('')
+	const [isModalVisible, setIsModalVisible] = useState(false)
+	const [selectedAppointment, setSelectedAppointment] = useState<Appointment | null>(null)
+
+	const showAppointmentDetails = (appointment: Appointment) => {
+		setSelectedAppointment(appointment)
+		setIsModalVisible(true)
+	}
+
+	const handleCancel = () => {
+		setIsModalVisible(false)
+	}
 
 	const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setSearch(e.target.value)
@@ -24,21 +35,46 @@ export function CalendarList() {
 
 	return (
 		<Space className="calendar-list" direction="vertical">
-			{' '}
-			{/* Utilisez la même classe CSS */}
 			<Input
-				placeholder="Rechercher des rendez-vous"
+				placeholder="Rechercher mes rendez-vous"
 				prefix={<SearchIcon />}
 				onChange={handleSearchChange}
 			/>
 			<List
 				dataSource={filteredAppointments}
 				renderItem={(item) => (
-					<List.Item key={item.name}>
-						<List.Item.Meta className="calendar-list__item" title={item.name} />
+					<List.Item key={item.name} onClick={() => showAppointmentDetails(item)}>
+						<List.Item.Meta
+							className="favorites-list__item"
+							title={item.name}
+							description={
+								<Flex justify="space-between">
+									<i>
+										{item.position} - {item.date.format('DD/MM/YYYY')}
+									</i>
+								</Flex>
+							}
+						/>
 					</List.Item>
 				)}
 			/>
+			<Modal
+				title="Les détails de l'événement :"
+				visible={isModalVisible}
+				onCancel={handleCancel}
+				footer={null}
+			>
+				{selectedAppointment && (
+					<div>
+						<p>Type: {selectedAppointment.type}</p>
+						<p>Name: {selectedAppointment.name}</p>
+						<p>Description: {selectedAppointment.description}</p>
+						<p>Position: {selectedAppointment.position}</p>
+						<p>Date: {selectedAppointment.date.format('DD/MM/YYYY')}</p>
+						<p>Participant: {selectedAppointment.participant}</p>
+					</div>
+				)}
+			</Modal>
 		</Space>
 	)
 }

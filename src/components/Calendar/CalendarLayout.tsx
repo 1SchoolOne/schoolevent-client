605d1a46c9d34691fb39c600674c6a7f6f584c8d
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-01-22 11:10:45 +0100

    fix: fix bug

--- src/components/Calendar/CalendarLayout.tsx
+++ src/components/Calendar/CalendarLayout.tsx
@@ -4,12 +4,11 @@ import { Calendar, CalendarList } from './_components'
 
 import './CalendarLayout-styles.less'
 
-const { Content, Header, Sider } = Layout
+const { Content, Sider } = Layout
 
 export function CalendarLayout() {
 	return (
 		<Layout className="calendar-layout">
-			<Header>Calendrier</Header>
 			<Layout>
 				<Sider width={250}>
 					<CalendarList />

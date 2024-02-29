2a167d86b353ad5a2e0afedf9f65a9c8f0f609ea
IlhanYapici <ilhan-yapici@outlook.fr>
2024-02-29 14:23:57 +0100

    refactor: make AuthLayout responsive

--- src/components/AuthLayout/AuthLayout.tsx
+++ src/components/AuthLayout/AuthLayout.tsx
@@ -6,15 +6,25 @@ import './AuthLayout-styles.less'
 
 export function AuthLayout() {
 	return (
-		<div className="login-container">
-			<div className="login-container__hero">
-				<img src="schoolevent_logo_white.svg" />
-				<img src="schoolevent_text_white.svg" />
+		<div className="auth-layout">
+			<header className="auth-layout__hero">
+				<div className="auth-layout__hero__logo-container">
+					<img
+						className="auth-layout__hero__logo-container__logo"
+						src="schoolevent_logo_white.svg"
+						alt="SchoolEvent logo"
+					/>
+					<img
+						className="auth-layout__hero__logo-container__text"
+						src="schoolevent_text_white.svg"
+						alt="SchoolEvent text logo"
+					/>
+				</div>
 				<p>
 					Le CRM <span>rapide</span> et <span>intuitif</span> dédié aux écoles.
 				</p>
-			</div>
-			<div className="login-container__form">
+			</header>
+			<div className="auth-layout__content">
 				<Card title={<h2>Connexion</h2>}>
 					<LoginForm />
 				</Card>

1b720049c39bfe182dbd30d1d66a6b40ba03ca74
IlhanYapici <ilhan-yapici@outlook.fr>
2024-03-07 11:29:48 +0100

    refactor: add icons to auth form submit buttons

--- src/components/MainLayout/_components/NotApproved/NotApproved.tsx
+++ src/components/MainLayout/_components/NotApproved/NotApproved.tsx
@@ -1,4 +1,4 @@
-import { ShieldWarning as ShieldIcon } from '@phosphor-icons/react'
+import { ArrowLeft as BackIcon, ShieldWarning as ShieldIcon } from '@phosphor-icons/react'
 import { Button, Typography } from 'antd'
 
 import { useSupabase } from '@utils'
@@ -12,12 +12,21 @@ export function NotApproved() {
 
 	return (
 		<main className="not-approved">
-			<ShieldIcon size={64} />
-			<Typography.Title className="not-approved__title">Accès refusé</Typography.Title>
+			<Typography.Text type="danger">
+				<ShieldIcon size={64} />
+			</Typography.Text>
+			<Typography.Title className="not-approved__title" type="danger">
+				Accès refusé
+			</Typography.Title>
 			<Typography.Paragraph>
 				Votre compte n'est pas approuvé. Veuillez contacter votre administrateur.
 			</Typography.Paragraph>
-			<Button className="not-approved__btn" type="primary" onClick={onClickFn}>
+			<Button
+				className="not-approved__btn"
+				type="primary"
+				icon={<BackIcon size={16} weight="bold" />}
+				onClick={onClickFn}
+			>
 				Retour à la page de connexion
 			</Button>
 		</main>

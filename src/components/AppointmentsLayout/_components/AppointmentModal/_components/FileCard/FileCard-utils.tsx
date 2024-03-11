ecac9548bfa13ad3ed72eb402553a7e00a84082c
IlhanYapici <ilhan-yapici@outlook.fr>
2024-03-11 16:15:30 +0100

    refactor: new file icons + custom error

--- src/components/AppointmentsLayout/_components/AppointmentModal/_components/FileCard/FileCard-utils.tsx
+++ src/components/AppointmentsLayout/_components/AppointmentModal/_components/FileCard/FileCard-utils.tsx
@@ -1,4 +1,16 @@
-import { File, FileImage, FilePdf, FileVideo } from '@phosphor-icons/react'
+import {
+	File,
+	FileArchive,
+	FileCsv,
+	FileDoc,
+	FileImage,
+	FilePdf,
+	FilePpt,
+	FileSvg,
+	FileText,
+	FileVideo,
+	FileXls,
+} from '@phosphor-icons/react'
 
 export function formatFileSize(bytes: number): string {
 	const units = ['B', 'KB', 'MB', 'GB', 'TB']
@@ -13,14 +25,30 @@ export function formatFileSize(bytes: number): string {
 	return bytes.toFixed(2) + ' ' + units[i]
 }
 
-export function getFileIcon(mimeType: string, size = 64) {
-	if (mimeType.startsWith('image')) {
-		return <FileImage size={size} weight="thin" />
-	} else if (mimeType === 'application/pdf') {
-		return <FilePdf size={size} weight="thin" />
-	} else if (mimeType === 'video/mp4') {
-		return <FileVideo size={size} weight="thin" />
+export function getFileIcon(fileName: string, size = 64) {
+	const iconColor = 'var(--ant-color-text-placeholder)'
+
+	if (fileName.endsWith('.png') || fileName.endsWith('.jpg') || fileName.endsWith('.jpeg')) {
+		return <FileImage size={size} weight="thin" color={iconColor} />
+	} else if (fileName.endsWith('.svg')) {
+		return <FileSvg size={size} weight="thin" color={iconColor} />
+	} else if (fileName.endsWith('.pdf')) {
+		return <FilePdf size={size} weight="thin" color={iconColor} />
+	} else if (fileName.endsWith('.mp4')) {
+		return <FileVideo size={size} weight="thin" color={iconColor} />
+	} else if (fileName.endsWith('.xls') || fileName.endsWith('.xlsx') || fileName.endsWith('.ods')) {
+		return <FileXls size={size} weight="thin" color={iconColor} />
+	} else if (fileName.endsWith('.csv')) {
+		return <FileCsv size={size} weight="thin" color={iconColor} />
+	} else if (fileName.endsWith('.doc') || fileName.endsWith('.docx') || fileName.endsWith('.odt')) {
+		return <FileDoc size={size} weight="thin" color={iconColor} />
+	} else if (fileName.endsWith('.ppt') || fileName.endsWith('.pptx') || fileName.endsWith('.odp')) {
+		return <FilePpt size={size} weight="thin" color={iconColor} />
+	} else if (fileName.endsWith('.rar') || fileName.endsWith('.zip')) {
+		return <FileArchive size={size} weight="thin" color={iconColor} />
+	} else if (fileName.endsWith('.txt')) {
+		return <FileText size={size} weight="thin" color={iconColor} />
 	}
 
-	return <File size={size} />
+	return <File size={size} weight="thin" color={iconColor} />
 }

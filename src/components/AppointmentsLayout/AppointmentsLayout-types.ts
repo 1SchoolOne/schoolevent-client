ec775a5cd34c84db5e7b237beb5db3cbbeed960e
IlhanYapici <ilhan-yapici@outlook.fr>
2024-03-18 14:48:31 +0100

    fix: remove useless props

--- src/components/AppointmentsLayout/AppointmentsLayout-types.ts
+++ src/components/AppointmentsLayout/AppointmentsLayout-types.ts
@@ -21,29 +21,3 @@ export interface IDropZoneProps {
 export interface IModalProps extends Omit<ModalProps, 'open' | 'centered' | 'onCancel'> {
 	onCancelCallback?: () => void
 }
-
-export type TAppointmentModalProps =
-	| INewAppointmentModalProps
-	| IEditAppointmentModalProps
-	| IViewAppointmentModalProps
-
-interface INewAppointmentModalProps {
-	mode: 'new'
-	appointmentId?: never
-	schoolId: string | null
-	status: Database['public']['Enums']['apt_status'] | null
-}
-
-interface IEditAppointmentModalProps {
-	mode: 'edit'
-	appointmentId: string | null
-	schoolId?: never
-	status?: never
-}
-
-interface IViewAppointmentModalProps {
-	mode: 'view'
-	appointmentId: string | null
-	schoolId?: never
-	status?: never
-}
--- src/components/AppointmentsLayout/_components/AppointmentModal/AppointmentModal.tsx
+++ src/components/AppointmentsLayout/_components/AppointmentModal/AppointmentModal.tsx
@@ -1,16 +1,17 @@
-import { TAppointmentModalProps } from '../../AppointmentsLayout-types'
+import { useAppointmentForm } from '@contexts'
+
 import { EditModal, NewModal, ViewModal } from './_components'
 
 import './AppointmentModal-styles.less'
 
-export function AppointmentModal(props: TAppointmentModalProps) {
-	const { mode, appointmentId, schoolId, status } = props
+export function AppointmentModal() {
+	const { mode } = useAppointmentForm()
 
 	if (mode === 'view') {
-		return <ViewModal appointmentId={appointmentId} />
+		return <ViewModal />
 	} else if (mode === 'edit') {
-		return <EditModal appointmentId={appointmentId} />
+		return <EditModal />
 	}
 
-	return <NewModal schoolId={schoolId} status={status} />
+	return <NewModal />
 }

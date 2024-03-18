a647681ee4628a4351163e9d89e9b063471d1c9a
IlhanYapici <ilhan-yapici@outlook.fr>
2024-03-18 17:43:44 +0100

    refactor: use icons when the key has a corresponding icon

---
+++ src/components/Keyboard/Keyboard-types.ts
@@ -0,0 +1,33 @@
+type TKey = 'ctrl' | 'return' | 'backspace' | 'super' | 'alt' | 'shift' | AlphaChar
+type AlphaChar =
+	| 'A'
+	| 'B'
+	| 'C'
+	| 'D'
+	| 'E'
+	| 'F'
+	| 'G'
+	| 'H'
+	| 'I'
+	| 'J'
+	| 'K'
+	| 'L'
+	| 'M'
+	| 'N'
+	| 'O'
+	| 'P'
+	| 'Q'
+	| 'R'
+	| 'S'
+	| 'T'
+	| 'U'
+	| 'V'
+	| 'W'
+	| 'X'
+	| 'Y'
+	| 'Z'
+
+export interface IKeyboardProps {
+	className?: string
+	combination: TKey[]
+}

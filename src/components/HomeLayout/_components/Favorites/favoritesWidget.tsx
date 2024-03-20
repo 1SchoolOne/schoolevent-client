908214f1b442365209ae5f89dbef7c7f781a65da
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-03-20 17:31:32 +0100

    Feat: Ajouter le widget Favoris avec les styles et la logique associée

---
+++ src/components/HomeLayout/_components/Favorites/favoritesWidget.tsx
@@ -0,0 +1,37 @@
+import { Card, List, Flex } from 'antd';
+import { useFavorites } from '@contexts';
+
+import './favoritesWidget-styles.less';
+export const FavoritesWidget: React.FC = () => {
+	const { favorites } = useFavorites();
+	const firstFourFavorites = favorites.slice(0, 4);
+
+	return (
+		<Card title="Écoles favorites" size="small" bordered={true}>
+			<div className="favorites-widget">
+				<List
+					className="favorites-widget__list"
+					locale={{
+						emptyText: 'Aucun favoris',
+					}}
+					dataSource={firstFourFavorites}
+					renderItem={(item) => (
+						<List.Item key={item.id}>
+							<List.Item.Meta
+								className="favorites-widget__item"
+								title={item.school_name}
+								description={
+									<Flex justify="space-between">
+										<i>
+											{item.school_city} - {item.school_postal_code}
+										</i>
+									</Flex>
+								}
+							/>
+						</List.Item>
+					)}
+				/>
+			</div>
+		</Card>
+	);
+};

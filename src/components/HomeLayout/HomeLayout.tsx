5f9bb9dfaaea31bd0dc5f4362883a5c51b9f1f72
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-03-23 17:39:52 +0100

    Mise à jour de la mise en page d'accueil

--- src/components/HomeLayout/HomeLayout.tsx
+++ src/components/HomeLayout/HomeLayout.tsx
@@ -1,45 +1,39 @@
-import { BasicLayout } from '@components';
-import { Col, Row } from 'antd';
+import { Col, Row } from 'antd'
+
+import { BasicLayout } from '@components'
+
+import { AppointmentsWidget } from './_components/Appointment/appointmentWidget'
+import { CalendarWidget } from './_components/Calendar/calendarWidget'
+import { VisitsWidget } from './_components/Event/visitsWidget'
+import { VisitsMonthWidget } from './_components/Event/visitsmonthWidget'
+import { FavoritesWidget } from './_components/Favorites/favoritesWidget'
+import { StudentWidget } from './_components/Student/studentWidget'
+
 import './HomeLayout-styles.less'
 
-import { FavoritesWidget } from './_components/Favorites/favoritesWidget';
-
-import { AppointmentsWidget } from './_components/Appointment/appointmentWidget';
-
-import { VisitsWidget } from './_components/Event/visitsWidget';
-import { VisitsMonthWidget } from './_components/Event/visitsmonthWidget';
-
-import { StudentWidget } from './_components/Student/studentWidget';
-
-import { CalendarWidget } from './_components/Calendar/calendarWidget';
-
 export function HomeLayout() {
-    return (
-        <BasicLayout
-            className="home-layout"
-            contentClassName="home-layout__content"
-        >
-            <Row gutter={16}>
-                <Col span={16}>
-                    <AppointmentsWidget appointments={[]} />
-                </Col>
-                <Col span={8}>
-                    <FavoritesWidget />
-                </Col>
-                <Col span={6}>
-                    <CalendarWidget />
-                </Col>
-                <Col span={6}>
-                    <VisitsWidget />
-                </Col>
-                <Col span={6}>
-                    <VisitsMonthWidget />
-                </Col>
-                <Col span={6}>
-                    <StudentWidget />
-                </Col>
-
-            </Row>
-        </BasicLayout>
-    )
+	return (
+		<BasicLayout className="home-layout" contentClassName="home-layout__content">
+			<Row gutter={[16, 16]}>
+				<Col span={18}>
+					<AppointmentsWidget appointments={[]} />
+				</Col>
+				<Col span={6}>
+					<FavoritesWidget />
+				</Col>
+				<Col span={6}>
+					<VisitsWidget />
+				</Col>
+				<Col span={6}>
+					<VisitsMonthWidget />
+				</Col>
+				<Col span={6}>
+					<StudentWidget />
+				</Col>
+				<Col span={6}>
+					<CalendarWidget />
+				</Col>
+			</Row>
+		</BasicLayout>
+	)
 }

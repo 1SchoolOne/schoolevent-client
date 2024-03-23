528ba9b5ebe3de58a52f8dd3f0c139af50b451ac
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-03-23 17:39:16 +0100

    Mise à jour du style et de la mise en page du widget étudiant

--- src/components/HomeLayout/_components/Student/studentWidget.tsx
+++ src/components/HomeLayout/_components/Student/studentWidget.tsx
@@ -1,43 +1,52 @@
-import { Card, Row, Col, Statistic } from 'antd';
-import { valueType } from 'antd/lib/statistic/utils';
-import { EyeOutlined, ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
-import { useState } from 'react';
-import CountUp from 'react-countup';
-import './studentWidget-styles.less';
+import { ArrowDownOutlined, ArrowUpOutlined, EyeOutlined } from '@ant-design/icons'
+import { Card, Col, Row, Statistic } from 'antd'
+import { valueType } from 'antd/lib/statistic/utils'
+import { useState } from 'react'
+import CountUp from 'react-countup'
+
+import './studentWidget-styles.less'
 
 const formatter = (value: valueType) => {
-  const numberValue = Number(value);
-  return isNaN(numberValue) ? value : <CountUp end={numberValue} separator="," />;
-};
+	const numberValue = Number(value)
+	return isNaN(numberValue) ? value : <CountUp end={numberValue} separator="," />
+}
 
 export const StudentWidget: React.FC = () => {
-  const [visits] = useState(67);
-  const [comparison] = useState(4);
+	const [visits] = useState(67)
+	const [comparison] = useState(4)
 
-
-  return (
-    <Card title="Inscriptions des nouveaux étudiants" size="small" bordered={true} className="student-widget">
-      <Row className="student-widget__item">
-        <Col span={2}>
-          <EyeOutlined />
-        </Col>
-        <Col span={22}>
-          <Statistic title="Nombre total de nouveaux étudiants" value={visits} formatter={formatter} />
-        </Col>
-      </Row>
-      <hr className="student-widget__separator" />
-      <Row className="student-widget__item">
-        <Col span={24}>
-          <Statistic
-            title="Comparé au mois dernier"
-            value={comparison}
-            formatter={formatter}
-            prefix={comparison > 0 ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
-            valueStyle={{ color: comparison > 0 ? '#3f8600' : '#cf1322' }}
-            suffix="%"
-          />
-        </Col>
-      </Row>
-    </Card>
-  );
-};
+	return (
+		<Card
+			title="Inscriptions des nouveaux étudiants"
+			size="small"
+			bordered={true}
+			className="student-widget"
+		>
+			<Row className="student-widget__item">
+				<Col span={2}>
+					<EyeOutlined />
+				</Col>
+				<Col span={22}>
+					<Statistic
+						title="Nombre total de nouveaux étudiants"
+						value={visits}
+						formatter={formatter}
+					/>
+				</Col>
+			</Row>
+			<hr className="student-widget__separator" />
+			<Row className="student-widget__item">
+				<Col span={24}>
+					<Statistic
+						title="Comparé au mois dernier"
+						value={comparison}
+						formatter={formatter}
+						prefix={comparison > 0 ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+						valueStyle={{ color: comparison > 0 ? '#3f8600' : '#cf1322' }}
+						suffix="%"
+					/>
+				</Col>
+			</Row>
+		</Card>
+	)
+}

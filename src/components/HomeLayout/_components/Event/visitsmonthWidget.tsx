3b58e31752fca3fa933cadb39c2520c01fafc6d7
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-03-23 16:22:35 +0100

    Mise à jour du widget de visites mensuelles***

--- src/components/HomeLayout/_components/Event/visitsmonthWidget.tsx
+++ src/components/HomeLayout/_components/Event/visitsmonthWidget.tsx
@@ -1,57 +1,49 @@
-import { Card, Typography, Row, Col, Button, Space } from 'antd';
+import { Card, Row, Col, Statistic, Button, Typography, Space, ConfigProvider } from 'antd';
+import { valueType } from 'antd/lib/statistic/utils';
 import { EyeOutlined, LeftOutlined, RightOutlined } from '@ant-design/icons';
-import { useState, useCallback } from 'react';
-import moment, { Moment } from 'moment';
-import 'moment/locale/fr';
+import { useState } from 'react';
+import CountUp from 'react-countup';
+import frFR from 'antd/lib/locale/fr_FR';
 import './visitsmonthWiedget-styles.less';
+const { Title } = Typography;
 
-moment.locale('fr');
-
-const ICON_SPAN_SIZE = 2;
-const TEXT_SPAN_SIZE = 22;
-
-const useMonth = (initialMonth: Moment) => {
-    const [month, setMonth] = useState(initialMonth);
-
-    const handlePrevMonth = useCallback(() => {
-        setMonth((prevMonth: Moment) => prevMonth.clone().subtract(1, 'month'));
-    }, []);
-
-    const handleNextMonth = useCallback(() => {
-        setMonth((nextMonth: Moment) => nextMonth.clone().add(1, 'month'));
-    }, []);
-
-    return { month, handlePrevMonth, handleNextMonth };
+const formatter = (value: valueType) => {
+  const numberValue = Number(value);
+  return isNaN(numberValue) ? value : <CountUp end={numberValue} separator="," />;
 };
 
 export const VisitsMonthWidget: React.FC = () => {
-    const [visits] = useState(78);
-    const { month, handlePrevMonth, handleNextMonth } = useMonth(moment());
+  const [visits] = useState(334);
+  const [currentMonth, setCurrentMonth] = useState(new Date());
 
-    return (
-      <Card title="Nombre de Visiteur" size="small" bordered={true} className="visitsmonth-widget">
-          <Row className="visitsmonth-widget__item">
-              <Col span={ICON_SPAN_SIZE}>
-                  <EyeOutlined/>
-              </Col>
-              <Col span={TEXT_SPAN_SIZE}>
-                  <Typography.Text>
-                      <span className="visitsmonth-widget__visits-number">{visits}</span> visiteur du mois
-                  </Typography.Text>
-              </Col>
-          </Row>
-          <hr className="visits-widget__separator"/>
-          <Row className="visits-widget__item">
-              <Col span={24}>
-                  <Space className="centered-content">
-                      <Button type="primary" icon={<LeftOutlined/>} onClick={handlePrevMonth}/>
-                      <Typography.Text>
-                          {month.format('MMMM')}
-                      </Typography.Text>
-                      <Button type="primary" icon={<RightOutlined/>} onClick={handleNextMonth}/>
-                  </Space>
-              </Col>
-          </Row>
+  const nextMonth = () => {
+    setCurrentMonth(new Date(currentMonth.setMonth(currentMonth.getMonth() + 1)));
+  };
+
+  const prevMonth = () => {
+    setCurrentMonth(new Date(currentMonth.setMonth(currentMonth.getMonth() - 1)));
+  };
+
+  return (
+    <ConfigProvider locale={frFR}>
+      <Card title="Nombre de participation par mois" size="small" bordered={true} className="visitsmonth-widget">
+        <Row>
+          <Col span={2}>
+            <EyeOutlined />
+          </Col>
+          <Col span={22}>
+            <Statistic title="Nombre total de participation du mois" value={visits} formatter={formatter} />
+          </Col>
+        </Row>
+        <hr/>
+        <Row justify="center" align="middle">
+          <Space size="large" align="center">
+            <Button icon={<LeftOutlined />} onClick={prevMonth} />
+            <Title level={4}>{currentMonth.toLocaleString('fr-FR', { month: 'long' })}</Title>
+            <Button icon={<RightOutlined />} onClick={nextMonth} />
+          </Space>
+        </Row>
       </Card>
-    );
+    </ConfigProvider>
+  );
 };

275b5bc1e72d1b50559bdb6c5f52daf368705149
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-03-23 16:22:46 +0100

    Ajouter le composant de calendrier avec les styles et les données d'appointements

---
+++ src/components/HomeLayout/_components/Calendar/calendarWidget.tsx
@@ -0,0 +1,29 @@
+import { Timeline, Card } from 'antd';
+import 'dayjs/locale/fr';
+import './calendarWidget-styles.less';
+
+export const CalendarWidget: React.FC = () => {
+	const startHour = 10;
+	const endHour = 21;
+	const timeSlots = [];
+
+	const appointments = [
+		{ title: 'Rencontre avec plein d étudiants.', time: 10 },
+		{ title: 'Salon avec des entrepreneurs et des patrons.', time: 14 },
+	];
+
+	for(let i = startHour; i <= endHour; i++) {
+		timeSlots.push(i);
+	}
+
+	const items = appointments.map(appointment => ({
+		label: <div className="timeline-item">{`${appointment.time}:00`}</div>,
+		children: <div className="timeline-item appointment-title">{appointment.title}</div>,
+	}));
+
+	return (
+		<Card title="Journée" size="small" bordered={false} className="calendar-widget">
+			<Timeline mode="left" items={items} />
+		</Card>
+	);
+};

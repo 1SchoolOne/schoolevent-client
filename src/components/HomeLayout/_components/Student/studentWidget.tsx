2cb00c837b1693101bd03473bb153b10d64af6b4
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-03-27 21:36:17 +0100

    Mise à jour des styles et du titre du widget étudiant

--- src/components/HomeLayout/_components/Student/studentWidget.tsx
+++ src/components/HomeLayout/_components/Student/studentWidget.tsx
@@ -1,10 +1,10 @@
-import { ArrowDownOutlined, ArrowUpOutlined, EyeOutlined } from '@ant-design/icons'
+import { ArrowDownOutlined, ArrowUpOutlined, UserAddOutlined } from '@ant-design/icons'
 import { Card, Col, Row, Statistic } from 'antd'
 import { valueType } from 'antd/lib/statistic/utils'
 import { useState } from 'react'
 import CountUp from 'react-countup'
 
-import './studentWidget-styles.less'
+import './StudentWidget-styles.less'
 
 const formatter = (value: valueType) => {
 	const numberValue = Number(value)
@@ -17,28 +17,24 @@ export const StudentWidget: React.FC = () => {
 
 	return (
 		<Card
-			title="Inscriptions des nouveaux étudiants"
+			title="Nombre d'inscriptions de nouveaux étudiants"
 			size="small"
 			bordered={true}
 			className="student-widget"
 		>
 			<Row className="student-widget__item">
-				<Col span={2}>
-					<EyeOutlined />
-				</Col>
-				<Col span={22}>
-					<Statistic
-						title="Nombre total de nouveaux étudiants"
-						value={visits}
-						formatter={formatter}
-					/>
-				</Col>
+				<Statistic
+					className="margin-bottom"
+					title="Nombre d'inscriptions d'étudiants :"
+					value={visits}
+					prefix={<UserAddOutlined />}
+				/>
 			</Row>
-			<hr className="student-widget__separator" />
+			<hr className="student-widget__separator margin-bottom" />
 			<Row className="student-widget__item">
 				<Col span={24}>
 					<Statistic
-						title="Comparé au mois dernier"
+						title="Comparé au mois dernier :"
 						value={comparison}
 						formatter={formatter}
 						prefix={comparison > 0 ? <ArrowUpOutlined /> : <ArrowDownOutlined />}

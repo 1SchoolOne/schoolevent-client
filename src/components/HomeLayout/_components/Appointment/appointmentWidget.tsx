72e2f27e7dc7479f3a6dde8ad953d1b9bef4426b
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-04-09 14:57:36 +0200

    Mise à jour du widget de rendez-vous pour utiliser useQuery et filtrer les rendez-vous par statut dans appointmentWidget.tsx

--- src/components/HomeLayout/_components/Appointment/appointmentWidget.tsx
+++ src/components/HomeLayout/_components/Appointment/appointmentWidget.tsx
@@ -1,15 +1,39 @@
+import { useQuery } from '@tanstack/react-query'
 import { Card, Col, List, Row, Typography } from 'antd'
+
+import { useAuth } from '@contexts'
+import { useSupabase } from '@utils'
 
 import '../../HomeLayout-styles.less'
 
-export const AppointmentsWidget: React.FC<{ appointments: any[] }> = ({ appointments }) => {
-	const toContactAppointments = appointments.filter(
-		(appointment) => appointment.status === 'to_contact',
+export function AppointmentsWidget() {
+	const supabase = useSupabase()
+	const { user } = useAuth()
+
+	const { data } = useQuery({
+		queryKey: ['appointments'],
+		queryFn: async () => {
+			const { data, error } = await supabase
+				.from('appointments')
+				.select('*')
+				.or(`assignee.eq.${user!.id},author_id.eq.${user!.id}`)
+
+			if (error) {
+				console.error('Error fetching appointments', error)
+				throw error
+			}
+
+			return data
+		},
+	})
+
+	const toContactAppointments = data?.filter(
+		(appointment) => appointment.apt_status === 'to_contact',
 	)
-	const contactedAppointments = appointments.filter(
-		(appointment) => appointment.status === 'contacted',
+	const contactedAppointments = data?.filter(
+		(appointment) => appointment.apt_status === 'contacted',
 	)
-	const plannedAppointments = appointments.filter((appointment) => appointment.status === 'planned')
+	const plannedAppointments = data?.filter((appointment) => appointment.apt_status === 'planned')
 
 	return (
 		<Card title="Rendez-vous" size="small" bordered={false} className="global-middle-widget">
@@ -18,21 +42,21 @@ export const AppointmentsWidget: React.FC<{ appointments: any[] }> = ({ appointm
 					<Typography.Title level={5}>À contacter</Typography.Title>
 					<List
 						dataSource={toContactAppointments}
-						renderItem={(item) => <List.Item>{item.name}</List.Item>}
+						renderItem={(item) => <List.Item>{JSON.stringify(item)}</List.Item>}
 					/>
 				</Col>
 				<Col xs={24} sm={8} className="category-container">
 					<Typography.Title level={5}>Contacté</Typography.Title>
 					<List
 						dataSource={contactedAppointments}
-						renderItem={(item) => <List.Item>{item.name}</List.Item>}
+						renderItem={(item) => <List.Item>{JSON.stringify(item)}</List.Item>}
 					/>
 				</Col>
 				<Col xs={24} sm={8}>
 					<Typography.Title level={5}>Rendez-vous planifié</Typography.Title>
 					<List
 						dataSource={plannedAppointments}
-						renderItem={(item) => <List.Item>{item.name}</List.Item>}
+						renderItem={(item) => <List.Item>{JSON.stringify(item)}</List.Item>}
 					/>
 				</Col>
 			</Row>

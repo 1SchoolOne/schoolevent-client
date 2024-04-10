e35963a1ee493c1ece3ff5adddb70e582a4102dc
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-04-10 19:09:04 +0200

    Réorganiser les styles du calendrier et des événements, trier et filtrer les événements et les rendez-vous, ajouter des options de menu pour modifier les rendez-vous et les événements, mettre à jour les librairies, et améliorer la lisibilité, la maintenabilité et la performance du code.

--- src/components/CalendarLayout/_components/CalendarList/CalendarList.tsx
+++ src/components/CalendarLayout/_components/CalendarList/CalendarList.tsx
@@ -1,72 +1,106 @@
-import { MagnifyingGlass as SearchIcon } from '@phosphor-icons/react'
-import { Button, Flex, Input, List, Space } from 'antd'
+import {
+	DotsThreeVertical as MoreIcon,
+	PencilSimple as PencilSimpleIcon,
+	MagnifyingGlass as SearchIcon,
+} from '@phosphor-icons/react'
+import { Badge, Dropdown, Flex, Input, List, Segmented, Space } from 'antd'
+import { ItemType } from 'antd/lib/menu/hooks/useItems'
 import dayjs from 'dayjs'
 import 'dayjs/locale/fr'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { IconButton } from '@components'
 
 import { ICalendarListProps } from './CalendarList-types'
 
+import '../../../FavoritesList/FavoritesList-styles.less'
 import './CalendarList-styles.less'
 
 export function CalendarList(props: ICalendarListProps) {
 	const { events, appointments } = props
 	const [search, setSearch] = useState('')
-	const [selectedButton, setSelectedButton] = useState('appointments')
+	const [selectedButton, setSelectedButton] = useState('Rendez-vous')
+	const navigate = useNavigate()
+	const currentDate = dayjs()
 
+	const sortedAndFilteredEvents = events
+		.filter((event) => dayjs(event.event_date).isAfter(currentDate))
+		.sort((a, b) => dayjs(a.event_date).valueOf() - dayjs(b.event_date).valueOf())
+
+	const sortedAndFilteredAppointments = appointments
+		.filter((appointment) => dayjs(appointment.planned_date).isAfter(currentDate))
+		.sort((a, b) => dayjs(a.planned_date).valueOf() - dayjs(b.planned_date).valueOf())
 	const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setSearch(e.target.value)
 	}
 
-	const handleButtonClick = (button: string) => {
-		setSelectedButton(button)
+	const handleSegmentChange = (value: string | number) => {
+		if (typeof value === 'string') {
+			setSelectedButton(value)
+		}
 	}
 
-	const filteredEvents = events.filter((event) => {
-		const matchName = event.event_title.toLowerCase().includes(search.toLowerCase())
-		const matchPosition = event.event_address.toLowerCase().includes(search.toLowerCase())
-		const matchDate = event.event_date
-			? event.event_date.toString().toLowerCase().includes(search.toLowerCase())
-			: false
-		return matchName || matchPosition || matchDate
-	})
+	const getMenuAppointments = (id: number): ItemType[] => [
+		{
+			key: 'create-follow-up',
+			label: 'Modifier le rendez-vous',
+			icon: <PencilSimpleIcon size={16} weight="bold" />,
+			onClick: () => navigate(`/appointments?action=edit&id=${id}`),
+		},
+	]
 
-	const filteredAppointments = appointments.filter((appointment) => {
-		const matchName = appointment.school_name.toLowerCase().includes(search.toLowerCase())
-		const matchPosition = appointment.school_city.toLowerCase().includes(search.toLowerCase())
-		const matchDate = appointment.planned_date
-			? appointment.planned_date.toString().toLowerCase().includes(search.toLowerCase())
-			: false
-		return matchName || matchPosition || matchDate
-	})
+	const getMenuEvents = (id: string): ItemType[] => [
+		{
+			key: 'create-follow-up',
+			label: "Modifier l'événement",
+			icon: <PencilSimpleIcon size={16} weight="bold" />,
+			// onClick: () => navigate(`/appointments?action=edit&id=${id}`),
+		},
+	]
+
+	const RendezVousOption = () => (
+		<div style={{ display: 'flex', alignItems: 'center' }}>
+			<Badge color="orange" />
+			<div style={{ marginLeft: '5px' }}>Rendez-vous</div>
+		</div>
+	)
+
+	const EvenementOption = () => (
+		<div style={{ display: 'flex', alignItems: 'center' }}>
+			<Badge color="blue" />
+			<div style={{ marginLeft: '5px' }}>Événement</div>
+		</div>
+	)
 
 	return (
 		<Space className="calendar-list" direction="vertical">
 			<Input
+				className="calendar-list__input"
 				placeholder={
-					selectedButton === 'appointments'
+					selectedButton === 'Rendez-vous'
 						? 'Rechercher mes rendez-vous'
 						: 'Rechercher mes événements'
 				}
 				prefix={<SearchIcon />}
 				onChange={handleSearchChange}
 			/>
-			<Space>
-				<Button
-					type={selectedButton === 'appointments' ? 'primary' : 'default'}
-					onClick={() => handleButtonClick('appointments')}
-				>
-					Rendez-vous
-				</Button>
-				<Button
-					type={selectedButton === 'events' ? 'primary' : 'default'}
-					onClick={() => handleButtonClick('events')}
-				>
-					Événements
-				</Button>
-			</Space>
-			{selectedButton === 'appointments' && (
+			<Segmented
+				className="calendar-list__segmented"
+				options={[
+					{ label: <RendezVousOption />, value: 'Rendez-vous' },
+					{ label: <EvenementOption />, value: 'Événement' },
+				]}
+				value={selectedButton}
+				onChange={handleSegmentChange}
+			/>
+			{selectedButton === 'Rendez-vous' && (
 				<List
-					dataSource={filteredAppointments}
+					className="favorites-list__list"
+					locale={{
+						emptyText: 'Aucun rendez-vous',
+					}}
+					dataSource={sortedAndFilteredAppointments}
 					renderItem={(item) => (
 						<List.Item key={item.school_name}>
 							<List.Item.Meta
@@ -76,8 +110,14 @@ export function CalendarList(props: ICalendarListProps) {
 									<Flex justify="space-between">
 										<i>
 											{item.school_city} -{' '}
+											<div>
+												{item.planned_date ? dayjs(item.planned_date).format('HH:mm') : 'N/A'}
+											</div>
 											{item.planned_date ? dayjs(item.planned_date).format('DD/MM/YYYY') : 'N/A'}
 										</i>
+										<Dropdown menu={{ items: getMenuAppointments(item.id) }} trigger={['click']}>
+											<IconButton type="text" icon={<MoreIcon size={16} weight="bold" />} />
+										</Dropdown>
 									</Flex>
 								}
 							/>
@@ -85,9 +125,13 @@ export function CalendarList(props: ICalendarListProps) {
 					)}
 				/>
 			)}
-			{selectedButton === 'events' && (
+			{selectedButton === 'Événement' && (
 				<List
-					dataSource={filteredEvents}
+					className="favorites-list__list"
+					locale={{
+						emptyText: 'Aucun événement',
+					}}
+					dataSource={sortedAndFilteredEvents}
 					renderItem={(item) => (
 						<List.Item key={item.event_title}>
 							<List.Item.Meta
@@ -97,8 +141,12 @@ export function CalendarList(props: ICalendarListProps) {
 									<Flex justify="space-between">
 										<i>
 											{item.event_address} -{' '}
+											<div>{item.event_date ? dayjs(item.event_date).format('HH:mm') : 'N/A'}</div>
 											{item.event_date ? dayjs(item.event_date).format('DD/MM/YYYY') : 'N/A'}
 										</i>
+										<Dropdown menu={{ items: getMenuEvents(item.id) }} trigger={['click']}>
+											<IconButton type="text" icon={<MoreIcon size={16} weight="bold" />} />
+										</Dropdown>
 									</Flex>
 								}
 							/>

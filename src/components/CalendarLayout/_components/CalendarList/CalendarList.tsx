e5226fb614c9c671bf2cf718cd82d16a440621d7
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-04-10 13:00:16 +0200

    Réorganiser les styles du calendrier et des événements

--- src/components/CalendarLayout/_components/CalendarList/CalendarList.tsx
+++ src/components/CalendarLayout/_components/CalendarList/CalendarList.tsx
@@ -1,80 +1,111 @@
 import { MagnifyingGlass as SearchIcon } from '@phosphor-icons/react'
-import { Flex, Input, List, Modal, Space } from 'antd'
+import { Button, Flex, Input, List, Space } from 'antd'
+import dayjs from 'dayjs'
 import 'dayjs/locale/fr'
 import { useState } from 'react'
 
-import { Appointment, appointments } from '../appointment'
+import { ICalendarListProps } from './CalendarList-types'
 
 import './CalendarList-styles.less'
 
-export function CalendarList() {
+export function CalendarList(props: ICalendarListProps) {
+	const { events, appointments } = props
 	const [search, setSearch] = useState('')
-	const [isModalVisible, setIsModalVisible] = useState(false)
-	const [selectedAppointment, setSelectedAppointment] = useState<Appointment | null>(null)
-
-	const showAppointmentDetails = (appointment: Appointment) => {
-		setSelectedAppointment(appointment)
-		setIsModalVisible(true)
-	}
-
-	const handleCancel = () => {
-		setIsModalVisible(false)
-	}
+	const [selectedButton, setSelectedButton] = useState('appointments')
 
 	const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setSearch(e.target.value)
 	}
 
-	const filteredAppointments = appointments.filter((appointment) => {
-		const matchName = appointment.name.toLowerCase().includes(search.toLowerCase())
-		const matchDescription = appointment.description.toLowerCase().includes(search.toLowerCase())
-		const matchPosition = appointment.position.toLowerCase().includes(search.toLowerCase())
+	const handleButtonClick = (button: string) => {
+		setSelectedButton(button)
+	}
 
-		return matchName || matchDescription || matchPosition
+	const filteredEvents = events.filter((event) => {
+		const matchName = event.event_title.toLowerCase().includes(search.toLowerCase())
+		const matchPosition = event.event_address.toLowerCase().includes(search.toLowerCase())
+		const matchDate = event.event_date
+			? event.event_date.toString().toLowerCase().includes(search.toLowerCase())
+			: false
+		return matchName || matchPosition || matchDate
+	})
+
+	const filteredAppointments = appointments.filter((appointment) => {
+		const matchName = appointment.school_name.toLowerCase().includes(search.toLowerCase())
+		const matchPosition = appointment.school_city.toLowerCase().includes(search.toLowerCase())
+		const matchDate = appointment.planned_date
+			? appointment.planned_date.toString().toLowerCase().includes(search.toLowerCase())
+			: false
+		return matchName || matchPosition || matchDate
 	})
 
 	return (
 		<Space className="calendar-list" direction="vertical">
 			<Input
-				placeholder="Rechercher mes rendez-vous"
+				placeholder={
+					selectedButton === 'appointments'
+						? 'Rechercher mes rendez-vous'
+						: 'Rechercher mes événements'
+				}
 				prefix={<SearchIcon />}
 				onChange={handleSearchChange}
 			/>
-			<List
-				dataSource={filteredAppointments}
-				renderItem={(item) => (
-					<List.Item key={item.name} onClick={() => showAppointmentDetails(item)}>
-						<List.Item.Meta
-							className="favorites-list__item"
-							title={item.name}
-							description={
-								<Flex justify="space-between">
-									<i>
-										{item.position} - {item.date.format('DD/MM/YYYY')}
-									</i>
-								</Flex>
-							}
-						/>
-					</List.Item>
-				)}
-			/>
-			<Modal
-				title="Les détails de l'événement :"
-				visible={isModalVisible}
-				onCancel={handleCancel}
-				footer={null}
-			>
-				{selectedAppointment && (
-					<div>
-						<p>Type: {selectedAppointment.type}</p>
-						<p>Name: {selectedAppointment.name}</p>
-						<p>Description: {selectedAppointment.description}</p>
-						<p>Position: {selectedAppointment.position}</p>
-						<p>Date: {selectedAppointment.date.format('DD/MM/YYYY')}</p>
-						<p>Participant: {selectedAppointment.participant}</p>
-					</div>
-				)}
-			</Modal>
+			<Space>
+				<Button
+					type={selectedButton === 'appointments' ? 'primary' : 'default'}
+					onClick={() => handleButtonClick('appointments')}
+				>
+					Rendez-vous
+				</Button>
+				<Button
+					type={selectedButton === 'events' ? 'primary' : 'default'}
+					onClick={() => handleButtonClick('events')}
+				>
+					Événements
+				</Button>
+			</Space>
+			{selectedButton === 'appointments' && (
+				<List
+					dataSource={filteredAppointments}
+					renderItem={(item) => (
+						<List.Item key={item.school_name}>
+							<List.Item.Meta
+								className="favorites-list__item"
+								title={item.school_name}
+								description={
+									<Flex justify="space-between">
+										<i>
+											{item.school_city} -{' '}
+											{item.planned_date ? dayjs(item.planned_date).format('DD/MM/YYYY') : 'N/A'}
+										</i>
+									</Flex>
+								}
+							/>
+						</List.Item>
+					)}
+				/>
+			)}
+			{selectedButton === 'events' && (
+				<List
+					dataSource={filteredEvents}
+					renderItem={(item) => (
+						<List.Item key={item.event_title}>
+							<List.Item.Meta
+								className="favorites-list__item"
+								title={item.event_title}
+								description={
+									<Flex justify="space-between">
+										<i>
+											{item.event_address} -{' '}
+											{item.event_date ? dayjs(item.event_date).format('DD/MM/YYYY') : 'N/A'}
+										</i>
+									</Flex>
+								}
+							/>
+						</List.Item>
+					)}
+				/>
+			)}
 		</Space>
 	)
 }

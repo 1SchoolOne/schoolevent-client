1016f4ea62b0306621f1eb2f17df69640bcfed29
Mathieu DA MOTA LONGO <mathieudamotalongo@gmail.com>
2024-04-10 23:41:42 +0200

    Mise à jour du widget FavoritesWidget pour afficher les favoris en deux colonnes dans favoritesWidget.tsx

--- src/components/HomeLayout/_components/Favorites/favoritesWidget.tsx
+++ src/components/HomeLayout/_components/Favorites/favoritesWidget.tsx
@@ -1,4 +1,4 @@
-import { Card, List } from 'antd'
+import { Card, List, Col, Row } from 'antd'
 
 import { useFavorites } from '@contexts'
 
@@ -7,31 +7,54 @@ import './FavoritesWidget-styles.less'
 
 export const FavoritesWidget: React.FC = () => {
 	const { favorites } = useFavorites()
-	const firstFourFavorites = favorites.slice(0, 3)
+	const firstThreeFavorites = favorites.slice(0, 2)
+	const nextThreeFavorites = favorites.slice(2,4)
 
 	return (
 		<Card
 			title="Dernières écoles mises en favoris"
 			size="small"
 			bordered={false}
-			className="global-bottom-widget"
+			className="global-little-widget"
 		>
-			<List
-				className="favorites-widget__list"
-				locale={{
-					emptyText: 'Aucun favoris',
-				}}
-				dataSource={firstFourFavorites}
-				renderItem={(item) => (
-					<List.Item key={item.id}>
-						<List.Item.Meta
-							className="favorites-widget__item"
-							title={item.school_name}
-							description={`${item.school_city} - ${item.school_postal_code}`}
-						/>
-					</List.Item>
-				)}
-			/>
+			<Row gutter={16}>
+				<Col xs={24} sm={12}>
+					<List
+						className="favorites-widget__list"
+						locale={{
+							emptyText: 'Aucun favoris',
+						}}
+						dataSource={firstThreeFavorites}
+						renderItem={(item) => (
+							<List.Item key={item.id}>
+								<List.Item.Meta
+									className="favorites-widget__item"
+									title={item.school_name}
+									description={`${item.school_city} - ${item.school_postal_code}`}
+								/>
+							</List.Item>
+						)}
+					/>
+				</Col>
+				<Col xs={24} sm={12}>
+					<List
+						className="favorites-widget__list"
+						locale={{
+							emptyText: 'Aucun favoris',
+						}}
+						dataSource={nextThreeFavorites}
+						renderItem={(item) => (
+							<List.Item key={item.id}>
+								<List.Item.Meta
+									className="favorites-widget__item"
+									title={item.school_name}
+									description={`${item.school_city} - ${item.school_postal_code}`}
+								/>
+							</List.Item>
+						)}
+					/>
+				</Col>
+			</Row>
 		</Card>
 	)
 }

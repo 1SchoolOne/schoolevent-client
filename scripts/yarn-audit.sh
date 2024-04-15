797c9d10581e12a51fe70d0b6e92412f7adbaf99
IlhanYapici <ilhan-yapici@outlook.fr>
2024-04-15 11:12:43 +0200

    config: first draft of yarn audit job
    Run a script that will exit 0 if no vuln or moderate vuln are found (displays a warning in case of moderate vuln). Exit 1 if critical vuln are found.

---
+++ scripts/yarn-audit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+yarn audit
+
+if [ $? -eq 0 ]; then
+  echo "No critical vulnerabilities found."
+  exit 0
+elif [ $? -lt 8 ] then
+  echo "::warning Moderate vulnerabilities found."
+  exit 0
+else
+  echo "::error Critical vulnerabilities found."
+  exit 1
+fi

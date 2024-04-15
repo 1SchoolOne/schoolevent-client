cda3a7d06d588f7c5bbc643d1ae34ff5d39a871e
IlhanYapici <ilhan-yapici@outlook.fr>
2024-04-15 16:32:45 +0200

    refactor: export events components within an index file

---
+++ src/components/events/index.ts
@@ -0,0 +1,4 @@
+export { EventDetail } from './EventDetail/EventDetail'
+export { EventForm } from './EventForm/EventForm'
+export { EventList } from './EventsList/EventList'
+export { EventUpdateForm } from './EventUpdate/EventUpdateForm'

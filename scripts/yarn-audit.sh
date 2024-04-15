4c3500fc6ef0e1c4505b609e0ab1c3bb4a41f1eb
IlhanYapici <ilhan-yapici@outlook.fr>
2024-04-15 11:20:49 +0200

    fix: missing semi-colon in yarn-audit.sh

--- scripts/yarn-audit.sh
+++ scripts/yarn-audit.sh
@@ -5,7 +5,7 @@ yarn audit
 if [ $? -eq 0 ]; then
   echo "No critical vulnerabilities found."
   exit 0
-elif [ $? -lt 8 ] then
+elif [ $? -lt 8 ]; then
   echo "::warning Moderate vulnerabilities found."
   exit 0
 else

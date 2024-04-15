88e44aefdc05c5c5a3bb686e90a5a351432b71c3
IlhanYapici <ilhan-yapici@outlook.fr>
2024-04-15 11:23:29 +0200

    fix: missing closing colons in yarn-audit.sh

--- scripts/yarn-audit.sh
+++ scripts/yarn-audit.sh
@@ -6,9 +6,9 @@ if [ $? -eq 0 ]; then
   echo "No critical vulnerabilities found."
   exit 0
 elif [ $? -lt 8 ]; then
-  echo "::warning Moderate vulnerabilities found."
+  echo "::warning::Moderate vulnerabilities found."
   exit 0
 else
-  echo "::error Critical vulnerabilities found."
+  echo "::error::Critical vulnerabilities found."
   exit 1
 fi

1e6550261a42b37eb4fb7fbcdc4162545e050fd0
IlhanYapici <ilhan-yapici@outlook.fr>
2024-06-02 14:34:33 +0200

    feat: add a "create event" button on event list + display a Result component when there is no event yet

--- src/components/Events/EventList/EventList.tsx
+++ src/components/Events/EventList/EventList.tsx
@@ -1,4 +1,8 @@
+import { Plus as NewIcon, ArrowClockwise as RefreshIcon } from '@phosphor-icons/react'
+import { useQueryClient } from '@tanstack/react-query'
+import { Button, Result } from 'antd'
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { renderEventList, useGroupedEvents } from './EventList-utils'
 import { Skeleton } from './_components/Skeleton/Skeleton'
@@ -9,6 +13,8 @@ export function EventList() {
 	const [activeYear, setActiveYear] = useState<string | string[] | null>(null)
 	const [activeMonth, setActiveMonth] = useState<string | string[] | null>(null)
 	const { data: groupedEvents, isPending } = useGroupedEvents()
+	const navigate = useNavigate()
+	const queryClient = useQueryClient()
 
 	const hasEvents = !!groupedEvents && Object.entries(groupedEvents).length > 0
 
@@ -27,11 +33,34 @@ export function EventList() {
 		return <Skeleton />
 	}
 
-	return hasEvents
-		? renderEventList({
-				groupedEvents,
-				activeYear: { value: activeYear ?? [], onChange: setActiveYear },
-				activeMonth: { value: activeMonth ?? [], onChange: setActiveMonth },
-		  })
-		: 'Aucun événement disponible pour le moment.'
+	return (
+		<>
+			<div>
+				<Button type="primary" icon={<NewIcon size={16} />} onClick={() => navigate('/events/new')}>
+					Créer un événement
+				</Button>
+			</div>
+			{hasEvents ? (
+				renderEventList({
+					groupedEvents,
+					activeYear: { value: activeYear ?? [], onChange: setActiveYear },
+					activeMonth: { value: activeMonth ?? [], onChange: setActiveMonth },
+				})
+			) : (
+				<Result
+					className="event-list__no-data"
+					status="404"
+					subTitle="Aucun événement disponible pour le moment."
+					extra={
+						<Button
+							icon={<RefreshIcon size={16} />}
+							onClick={() => queryClient.resetQueries({ queryKey: ['events'] })}
+						>
+							Actualiser
+						</Button>
+					}
+				/>
+			)}
+		</>
+	)
 }

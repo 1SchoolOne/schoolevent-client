00257b74a195590b55f94ea19fd8b31b0bd8cfa7
Xamilu <makesou14@gmail.com>
2024-06-02 17:16:12 +0200

    feat: create reward's page front

--- src/components/StudentSection/Reward/Reward.tsx
+++ src/components/StudentSection/Reward/Reward.tsx
@@ -1,7 +1,23 @@
+import { Button, Typography } from 'antd'
+import './Reward-styles.less'
+import { HistoricEventCell } from './_components/HistoricEventCell/HistoricEventCell'
+
+const { Title } = Typography
+
 export function Reward() {
   return (
-    <div>
-      <h1>Hello World !</h1>
+    <div className="flex-container">
+      <div className="reward-container">
+        <Title level={2}>Convertis tes points !</Title>
+        <div className="convert">
+          <Title level={4}>Tes points : <span className="points">0</span></Title>
+          <Button className="convert-button">Convertir</Button>
+        </div>
+      </div>
+      <div className="historic-container">
+        <Title level={2}>Historique de tes évènements</Title>
+        <HistoricEventCell/>
+      </div>
     </div>
   )
 }

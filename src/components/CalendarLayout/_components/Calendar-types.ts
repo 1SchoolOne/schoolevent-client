e5db8a1ea800f220db625b9c1bf28f785c5293a7
MathieuDML <mathieudamotalongo@gmail.com>
2024-06-14 16:36:29 +0200

    refactor: Update CalendarLayout component to include user email in appointments and events

--- src/components/CalendarLayout/_components/Calendar-types.ts
+++ src/components/CalendarLayout/_components/Calendar-types.ts
@@ -1,12 +1,16 @@
 import { Database, TAppointment, TEvent } from '@types'
 
 export interface ICalendarProps {
-	appointments: TAppointment[]
-	events: TEvent[]
+	appointments: Array<TAppointment & { users: { email: string } | null }>
+	events: Array<TEvent & { users: { email: string } | null }>
 }
 
 export interface ICalendarListProps extends ICalendarProps {}
 
-export type AppointmentItem = Database['public']['Tables']['appointments']['Row']
+export type AppointmentItem = Database['public']['Tables']['appointments']['Row'] & {
+	users: { email: string } | null
+}
 
-export type EventItem = Database['public']['Tables']['events']['Row']
+export type EventItem = Database['public']['Tables']['events']['Row'] & {
+	users: { email: string } | null
+}

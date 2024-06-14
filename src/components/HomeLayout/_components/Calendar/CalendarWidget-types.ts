70d10609845e6ee27be404d8963db7de6d98be1d
MathieuDML <mathieudamotalongo@gmail.com>
2024-06-14 16:36:46 +0200

    refactor: Update CalendarWidget types to remove unnecessary semicolons

--- src/components/HomeLayout/_components/Calendar/CalendarWidget-types.ts
+++ src/components/HomeLayout/_components/Calendar/CalendarWidget-types.ts
@@ -1,10 +1,10 @@
 export interface TEvent {
-    event_title: string;
-    event_date: string | null;
+	event_title: string
+	event_date: string | null
 }
 
 export interface TAppointment {
-    school_name: string;
-    apt_status: string;
-    planned_date: string | null;
+	school_name: string
+	apt_status: string
+	planned_date: string | null
 }

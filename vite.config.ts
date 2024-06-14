306cd34771d76ea05ea28bbd3436e281d87cb87f
IlhanYapici <ilhan-yapici@outlook.fr>
2024-06-14 10:57:26 +0200

    config: upgrade vite packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({

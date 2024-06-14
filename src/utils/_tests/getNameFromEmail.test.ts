97f475c6a153154a3b71e4db74c2d04dea9e990d
IlhanYapici <ilhan-yapici@outlook.fr>
2024-06-14 11:55:18 +0200

    refactor: improve getNameFromEmail to handle complex email

--- src/utils/_tests/getNameFromEmail.test.ts
+++ src/utils/_tests/getNameFromEmail.test.ts
@@ -7,3 +7,19 @@ test('extract successfully name and initials from email', () => {
 	expect(formatted.name).equals('John DOE')
 	expect(formatted.initials).equals('JD')
 })
+
+test('extract successfully name and initials from complex email', () => {
+	const email = 'john-junior.super-doe@esiee-it.fr'
+	const formatted = getNameFromEmail(email)
+
+	expect(formatted.name).equals('John Junior SUPER DOE')
+	expect(formatted.initials).equals('JS')
+})
+
+test('extract name and initials successfully from a single name email', () => {
+	const email = 'john@esiee-it.fr'
+	const formatted = getNameFromEmail(email)
+
+	expect(formatted.name).equals('John')
+	expect(formatted.initials).equals('J')
+})

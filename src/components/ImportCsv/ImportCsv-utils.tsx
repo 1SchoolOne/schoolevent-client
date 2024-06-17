f6779c5a271a7c18f5156894cab33c4cc7250abf
MathieuDML <mathieudamotalongo@gmail.com>
2024-06-17 11:07:52 +0200

    Feat : ajout des utils pour l'import du csv

---
+++ src/components/ImportCsv/ImportCsv-utils.tsx
@@ -0,0 +1,53 @@
+import Papa from 'papaparse'
+
+import { ImportItem } from './ImportCsv-types'
+
+interface CSVRow {
+	Adresse: string | null
+	Ville: string
+	Latitude: string | null
+	Longitude: string | null
+	Email: string | null
+	CodePostal: string
+	NomEcole: string
+	TypeEcole: string
+	Telephone: string | null
+}
+
+const mapCSVToImportItem = (row: CSVRow, userId: string): ImportItem => {
+	return {
+		address: row.Adresse,
+		city: row.Ville,
+		created_at: new Date().toISOString(),
+		id: 0,
+		latitude: row.Latitude,
+		longitude: row.Longitude,
+		mail: row.Email,
+		postal_code: row.CodePostal,
+		school_name: row.NomEcole,
+		school_type: row.TypeEcole,
+		telephone: row.Telephone,
+		user_id: userId,
+	}
+}
+
+export const parseCsv = (file: File, userId: string): Promise<ImportItem[]> => {
+	return new Promise((resolve, reject) => {
+		Papa.parse<CSVRow>(file, {
+			header: true,
+			transformHeader: (header) => header.trim(),
+			complete: (results) => {
+				if (results.errors.length) {
+					reject(new Error(`Parsing errors: ${results.errors.map((e) => e.message).join(', ')}`))
+				} else {
+					try {
+						const data = results.data.map((row) => mapCSVToImportItem(row, userId))
+						resolve(data)
+					} catch (error) {
+						reject(new Error('Error mapping CSV data to ImportItem'))
+					}
+				}
+			},
+		})
+	})
+}

921f3a664fe80f23dee372bb67b81cdbaf568789
Xamilu <makesou14@gmail.com>
2024-06-19 17:04:35 +0200

    feat: historic reward cell front

--- src/components/StudentSection/index.ts
+++ src/components/StudentSection/index.ts
@@ -1,3 +1,2 @@
 export { Reward } from './Reward/Reward'
 export { ChoosingRewardLayout } from './Reward/_components/ChoosingRewardLayout/ChoosingRewardLayout'
-export { RewardForm } from './Reward/_components/RewardForm/RewardForm'

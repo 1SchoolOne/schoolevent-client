e32b6e574b20465da06400ef4e4fbf8871912ade
Xamilu <makesou14@gmail.com>
2024-06-19 20:01:37 +0200

    feat: get reward request from student (not finish)

--- src/components/StudentSection/Reward/_components/ChoosingRewardLayout/ChoosingReward-utils.tsx
+++ src/components/StudentSection/Reward/_components/ChoosingRewardLayout/ChoosingReward-utils.tsx
@@ -1,15 +1,65 @@
-import { TReward } from "@types"
+import { TReward } from '@types'
 import { log, useSupabase } from '@utils'
 
-export const useFetchRewardData = async(): Promise<TReward[]> => { 
-  const supabase = useSupabase()
+export const useFetchRewardData = async (): Promise<TReward[]> => {
+	const supabase = useSupabase()
 
-  const { data, error } = await supabase.from('rewards').select('*')
+	const { data, error } = await supabase.from('rewards').select('*')
 
-  if (error) {
-    log.error('Error fetching rewards: ', error)
-    return []
-  }
+	if (error) {
+		log.error('Error fetching rewards: ', error)
+		return []
+	}
 
-  return data
+	return data
+}
+
+export const useAddRewardSelection = async (userId: string, rewardId: number) => {
+	const supabase = useSupabase()
+
+	const claimedAt = new Date().toISOString()
+
+	const { data, error } = await supabase
+		.from('students_reward')
+		.insert([{ user_id: userId, reward_id: rewardId, quantity: 1, claimed_at: claimedAt }])
+
+	if (error) {
+		log.error('Error adding reward selection: ', error)
+		throw error
+	}
+
+	return data
+}
+
+export const useRemoveRewardSelection = async (userId: string, rewardId: number) => {
+	const supabase = useSupabase()
+
+	const { data, error } = await supabase
+		.from('students_reward')
+		.delete()
+		.eq('user_id', userId)
+		.eq('reward_id', rewardId)
+
+	if (error) {
+		log.error('Error removing reward selection: ', error)
+		throw error
+	}
+
+	return data
+}
+
+export const useFetchRewardSelections = async (userId: string) => {
+	const supabase = useSupabase()
+
+	const { data, error } = await supabase
+		.from('reward_selections')
+		.select('reward_id, quantity')
+		.eq('user_id', userId)
+
+	if (error) {
+		log.error('Error fetching reward selections: ', error)
+		throw error
+	}
+
+	return data
 }

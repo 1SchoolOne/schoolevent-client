6946e01a8f982bd05d957e34b921d01460d0d8e3
MathieuDML <mathieudamotalongo@gmail.com>
2024-06-24 12:02:50 +0200

    Fix : Remise au propre du code, modification des types et de la typographie

--- src/components/HomeLayout/_components/Event/NextEventWidget.tsx
+++ src/components/HomeLayout/_components/Event/NextEventWidget.tsx
@@ -1,84 +1,77 @@
-import { TeamOutlined } from '@ant-design/icons'
-import {Card, Col, Row, Statistic, Typography, Button, Space} from 'antd'
-import { useMemo } from 'react'
-import { useNavigate } from 'react-router-dom'
-import '../../HomeLayout-styles.less'
-
+import { CalendarOutlined, ClockCircleOutlined, TeamOutlined } from '@ant-design/icons'
+import { Button, Card, Col, Row, Space, Statistic, Typography } from 'antd'
 import dayjs from 'dayjs'
 import 'dayjs/locale/fr'
+import { useMemo } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-import {TAppointment, TEvent} from "./EventWidget-types";
+import { ICalendarProps } from '../Home-types'
 
+import '../../HomeLayout-styles.less'
 
-interface EventWidgetProps {
-	events: TEvent[];
-	appointments: TAppointment[];
-}
-
-export function NextEventWidget({ events, appointments }: EventWidgetProps) {
+export function NextEventWidget({ events, appointments }: ICalendarProps) {
 	const navigate = useNavigate()
 	const currentDate = dayjs()
-	const plannedAppointments = appointments?.filter((appointment) => appointment.apt_status === 'planned')
+	const plannedAppointments = appointments?.filter(
+		(appointment) => appointment.apt_status === 'planned',
+	)
 	const studentCount = useMemo(() => 3, [])
 
 	const nextEvent = events
 		? events
-			.filter((event) => dayjs(event.event_date).isAfter(currentDate))
-			.sort((a, b) => dayjs(a.event_date).isAfter(dayjs(b.event_date)) ? 1 : -1)
-			[0]
-		: null;
+				.filter((event) => dayjs(event.event_date).isAfter(currentDate))
+				.sort((a, b) => (dayjs(a.event_date).isAfter(dayjs(b.event_date)) ? 1 : -1))[0]
+		: null
 
 	const nextAppointment = plannedAppointments
 		? plannedAppointments
-			.filter((appointment) => dayjs(appointment.planned_date).isAfter(currentDate))
-			.sort((a, b) => dayjs(a.planned_date).isAfter(dayjs(b.planned_date)) ? 1 : -1)
-			[0]
-		: null;
+				.filter((appointment) => dayjs(appointment.planned_date).isAfter(currentDate))
+				.sort((a, b) => (dayjs(a.planned_date).isAfter(dayjs(b.planned_date)) ? 1 : -1))[0]
+		: null
 
 	return (
 		<Card
 			title={
 				<Space align="center">
-					Bonjour
+					Bonjour,
 					<Button type="primary" size="small" onClick={() => navigate(`/appointments?action=new`)}>
 						Créer un rendez-vous
 					</Button>
 					<Button type="primary" size="small" onClick={() => navigate(`/events/new`)}>
-						Créer un évenement
+						Créer un événement
 					</Button>
 				</Space>
 			}
-			className={'global-large-widget'}
+			className="global-large-widget"
 		>
-			<Row gutter={16}>
-
-				<Col xs={24} sm={24} md={8} lg={8} xl={8}>
-					<Typography className="ant-statistic-title">Votre prochain événement est :</Typography>
-					{nextEvent && (
+			<Row gutter={[16, 16]}>
+				<Col xs={24} sm={12} md={8}>
+					<Typography.Title level={5}>Votre prochain événement :</Typography.Title>
+					{nextEvent ? (
 						<Typography.Text>
-							{"Titre de l'événement : " + nextEvent.event_title}
+							<CalendarOutlined /> {nextEvent.event_title}
 							<br />
-							{"Date de l'événement : " + dayjs(nextEvent.event_date).format('DD/MM/YYYY - HH:mm')}
+							<ClockCircleOutlined /> {dayjs(nextEvent.event_date).format('DD/MM/YYYY - HH:mm')}
 						</Typography.Text>
+					) : (
+						<Typography.Text>Aucun événement à venir</Typography.Text>
 					)}
 				</Col>
-
-				<Col xs={24} sm={24} md={8} lg={8} xl={8}>
-					<Statistic
-						title="Nombre d'étudiants présents a l'événement :"
-						value={studentCount}
-						prefix={<TeamOutlined />}
-					/>
+				<Col xs={24} sm={12} md={8}>
+					<Typography.Title level={5}>Nombre d'étudiants présents à l'événement :</Typography.Title>
+					<Statistic value={studentCount} prefix={<TeamOutlined />} />
 				</Col>
-
-				<Col xs={24} sm={24} md={8} lg={8} xl={8}>
-					<Typography className="ant-statistic-title">Votre prochain rendez-vous est :</Typography>
-					{nextAppointment && (
+				<Col xs={24} sm={12} md={8}>
+					<Typography.Title level={5}>Votre prochain rendez-vous :</Typography.Title>
+					{nextAppointment ? (
 						<Typography.Text>
-							{"Nom de l'école : " + nextAppointment.school_name}
+							<CalendarOutlined /> {nextAppointment.school_name}
 							<br />
-							{"Date du rendez-vous : " + dayjs(nextAppointment.planned_date).format('DD/MM/YYYY - HH:mm')}
+							<ClockCircleOutlined />{' '}
+							{dayjs(nextAppointment.planned_date).format('DD/MM/YYYY - HH:mm')}
 						</Typography.Text>
+					) : (
+						<Typography.Text>Aucun rendez-vous à venir</Typography.Text>
 					)}
 				</Col>
 			</Row>

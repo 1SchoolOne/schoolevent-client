24a3d0e2469de8a3a160fc52dd421ec5b36ae013
IlhanYapici <ilhan-yapici@outlook.fr>
2024-06-25 18:29:25 +0200

    fix: prevent users from accessing new/edit event form

--- src/components/Events/EventForm/EventForm.tsx
+++ src/components/Events/EventForm/EventForm.tsx
@@ -1,6 +1,8 @@
-import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 import { BasicLayout } from '@components'
+import { useAuth } from '@contexts'
 
 import { Form } from './_components/Form/Form'
 
@@ -8,6 +10,18 @@ import './EventForm-styles.less'
 
 export function EventForm() {
 	const { eventId } = useParams()
+	const { role } = useAuth()
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		if (role === 'student') {
+			navigate('/events')
+		}
+	}, [role])
+
+	if (role === null) {
+		return null
+	}
 
 	return (
 		<BasicLayout className="event-form-layout" contentClassName="event-form-layout__content">

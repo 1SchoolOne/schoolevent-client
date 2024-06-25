1d5ba1e1af09a2a2683d6c411c8f98cc99f74f06
IlhanYapici <ilhan-yapici@outlook.fr>
2024-06-25 18:45:53 +0200

    refactor: update env variables to use test env in staging mode

--- src/utils/useSupabase.ts
+++ src/utils/useSupabase.ts
@@ -2,8 +2,16 @@ import { createClient } from '@supabase/supabase-js'
 
 import { Database } from '@types'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const APP_MODE = import.meta.env.MODE
+
+const supabaseUrl =
+	APP_MODE === 'staging'
+		? import.meta.env.VITE_TEST_SUPABASE_URL
+		: import.meta.env.VITE_SUPABASE_URL
+const supabaseKey =
+	APP_MODE === 'staging'
+		? import.meta.env.VITE_TEST_SUPABASE_KEY
+		: import.meta.env.VITE_SUPABASE_KEY
 
 const supabase = createClient<Database>(supabaseUrl, supabaseKey)
 

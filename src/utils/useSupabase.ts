b87cc2934df8b1dfd93c3930898c20de902451e0
IlhanYapici <ilhan-yapici@outlook.fr>
2024-06-26 22:47:44 +0200

    fix: useSupabase test mode variables

--- src/utils/useSupabase.ts
+++ src/utils/useSupabase.ts
@@ -3,15 +3,14 @@ import { createClient } from '@supabase/supabase-js'
 import { Database } from '@types'
 
 const APP_MODE = import.meta.env.MODE
+const IS_TEST_MODE = APP_MODE === 'staging' || APP_MODE === 'test'
 
-const supabaseUrl =
-	APP_MODE === 'staging'
-		? import.meta.env.VITE_TEST_SUPABASE_URL
-		: import.meta.env.VITE_SUPABASE_URL
-const supabaseKey =
-	APP_MODE === 'staging'
-		? import.meta.env.VITE_TEST_SUPABASE_KEY
-		: import.meta.env.VITE_SUPABASE_KEY
+const supabaseUrl = IS_TEST_MODE
+	? import.meta.env.VITE_TEST_SUPABASE_URL
+	: import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = IS_TEST_MODE
+	? import.meta.env.VITE_TEST_SUPABASE_KEY
+	: import.meta.env.VITE_SUPABASE_KEY
 
 const supabase = createClient<Database>(supabaseUrl, supabaseKey)
 
